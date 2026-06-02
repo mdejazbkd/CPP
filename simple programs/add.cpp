2081723c912febb53face9a90cd9074fa642e4c4
mdejazbkd@gmail.com <mdejazbkd@gmail.com>
2026-06-02 19:57:34 +0530

    first c++ programs

---
+++ simple programs/add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(){
+    //by using variables
+    int a=23;
+    int b=34;
+    int result = a+b;
+    std :: cout << result << '\n';
+
+    //using direct addition
+    std::cout<< a+b << std :: endl;
+
+    //direct addition using no variables
+    std :: cout << 53+34-4<< std:: endl;
+}
---
+++ simple programs/calculations.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main(){
+    std::cout<< 5+6 << std::endl;
+    std::cout<< 5*6 << "\n";
+    std::cout<< 6-5 << '\n';
+    std::cout<< 10/5;
+}
